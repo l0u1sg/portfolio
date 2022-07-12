99ea8c3fa2dfac8c0ee26858d27cc62fdc39c2e7
Louis Gaallet <me@louisgallet.fr>
2022-07-12 14:32:19 +0200

    feat: :construction: Starting website

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { Parallax, ParallaxLayer } from '@react-spring/parallax'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Parallax pages={2}>
+        <ParallaxLayer>
+          <p id="firstPage">Louis Gallet.</p>
+        </ParallaxLayer>
+        <ParallaxLayer offset={1} >
+          <h2>Test</h2>
+        </ParallaxLayer>
+      </Parallax>
     </div>
   );
 }
